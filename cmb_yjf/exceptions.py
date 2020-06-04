398626b25447957db1fa718b7c7c38abd15f9342
hanyi <jasonham.cn@outlook.com>
2020-06-04 17:50:30 +0800

    6-4

---
+++ cmb_yjf/exceptions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+    exceptions.py
+    ~~~~~~~~~~
+"""
+
+
+class CMBYJFException(Exception):
+    def __init__(self, code, message):
+        self.__code = code
+        self.__message = message
+
+    def to_unicode(self):
+        return "CMB_YJF_Exception: code:{}, message:{}".format(self.__code, self.__message)
+
+    def __str__(self):
+        return self.to_unicode()
+
+    def __repr__(self):
+        return self.to_unicode()
